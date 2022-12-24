2ac75dc7e6010112bbbf142cd3bf694c74965e6c
naudiyal30 <iamjatinnaudiyal@gmail.com>
2022-12-24 14:25:42 +0530

    Python basics

---
+++ day1/main.py
@@ -0,0 +1,42 @@
+#Escape sequence character
+print("how to win\nand influence \"people\"")
+
+
+#More on Print statement
+print("jatin", 89, 98, sep="~", end="naudiyal\n")
+print("crazy guy")
+
+
+#Variables
+a = 56787      #integer
+print(a)
+b = "jatin"     #string
+print(b)
+c = True    #boolean type
+print(c)
+d = None   #none type
+print(d)
+
+
+print("type of a, b, c, d is", type(a), type(b), type(c), type(d))      #To print data types
+
+
+list1 = [9, 3.4, [-5, 8], ["apple", "banana"]]      #list
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))        #tuple
+print(tuple1)
+
+dict1 = {"name":"jatin", "age":24, "canVote":True}      #dictonary
+print(dict1)
+
+
+#Operators
+print(15+8)
+print(15-8)
+print(15*8)
+print(15/8)
+print(15//8)
+print(15%8)
+print(15**3)
+
