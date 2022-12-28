28f803752bb1dec5d3f290df6c06de394c285a36
naudiyal30 <iamjatinnaudiyal@gmail.com>
2022-12-28 10:30:51 +0530

    Recursion

--- day4/main.py
+++ day4/main.py
@@ -5,3 +5,20 @@ def square(n):
 square(5)
 print(square.__doc__)
 
+
+#Recursion
+def factorial(n):
+     if(n==0 or n==1):
+          return 1
+     else:
+          return n * factorial(n-1)
+print(factorial(5))
+
+def fibonacci(num):
+  if (num ==0):
+    return 0
+  elif num ==1:
+    return 1
+  else:
+   return fibonacci(num-1) +  fibonacci(num-2)
+print(fibonacci(7))
