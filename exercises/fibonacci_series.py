0a4435c1c6752f1e6762ed2e379897762b46e0b5
naudiyal30 <iamjatinnaudiyal@gmail.com>
2022-12-29 18:43:48 +0530

    exercise added

---
+++ exercises/fibonacci_series.py
@@ -0,0 +1,21 @@
+def fiboIter(n):
+    prevNum = 0
+    currentNum = 1
+    for i in range(1, n):
+        prevPrevNum = prevNum
+        prevNum = currentNum
+        currentNum = prevNum + prevPrevNum
+    return currentNum
+
+def fiboRecur(n):
+    if n==0:
+        return 0
+    elif (n==1):
+        return 1
+    else:
+        return fiboRecur(n-1) + fiboRecur(n-2)
+
+if __name__ == "__main__":
+    num = int(input("Enter a number\n"))
+    print(f"Using recursion value of fib({num}) is {fiboRecur(num)}")
+    print(f"Using iteration value of fib({num}) is {fiboIter(num)}")
