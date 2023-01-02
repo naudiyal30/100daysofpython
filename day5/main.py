86d3b7b71e76b078c762cc5b96cca39fe704dd01
naudiyal30 <iamjatinnaudiyal@gmail.com>
2023-01-02 10:14:09 +0530

    day 5 added

---
+++ day5/main.py
@@ -0,0 +1,23 @@
+#for loop with else statement
+for i in range(5):
+    print(i)
+else:
+    print("Sorry no i")
+
+
+for i in range(6):
+    print(i)
+    if i==4:
+        break
+else:
+    print("Sorry no i")
+
+
+i = 0
+while i<7:
+    print(i)
+    i = i+ 1
+    if i==4:
+        break
+else:
+    print("Sorry no i")
