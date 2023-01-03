38c61a76615be40ca92c274b489eb35aee23c103
naudiyal30 <iamjatinnaudiyal@gmail.com>
2023-01-03 09:36:23 +0530

    Exception Handling

--- day5/main.py
+++ day5/main.py
@@ -21,3 +21,30 @@ while i<7:
         break
 else:
     print("Sorry no i")
+
+
+#Exceptional Handling
+a =input("Enter the number: ")
+print(f"Multiplication table of {a} is:")
+
+try:
+    for i in range (1, 11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+
+except:
+    print("Invalid Input!")
+
+print("Some imp lines of codes")
+print("End of program")
+
+
+try:
+    num=int(input("Enter an integer: \n"))
+    a =[6, 3]
+    print(a[num])
+
+except ValueError:
+    print("Number entered is not an integer")
+
+except IndexError:
+    print("Index Error")
