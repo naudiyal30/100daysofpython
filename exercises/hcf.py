928e4df215721fce56db7543743daf10101a2539
naudiyal30 <iamjatinnaudiyal@gmail.com>
2023-01-05 17:12:11 +0530

    exercise added

---
+++ exercises/hcf.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number\n"))
+num2 = int(input("Enter second number\n"))
+
+if num1>num2:
+    mn = num1
+else:
+    mn = num2
+
+for i in range(1, mn+1):
+    if num1%i==0 and num2%i==0:
+        hcf = i
+
+print(f"The HCF/GDF of these two numbers is {hcf}")
